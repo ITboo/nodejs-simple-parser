8c4cc2bb28ee6037130094019b1d86c85176cb82
ITboo <annaacdc@tut.by>
2024-06-01 20:32:41 +0300

    use fs module to write received data in txt file

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import puppeteer from "puppeteer";
+import { writeFile } from "node:fs/promises";
+
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://www.youtube.com/@UtopiaShow/videos/");
-  let arr = await page.evaluate(()=>{
-    let text = Array.from(document.querySelectorAll('#video-title'), el=>el.innerText);
+  await page.goto("https://www.youtube.com/@TheBadComedian/videos");
+  await page.setViewport({ width: 1080, height: 1024 });
+  let arr = await page.evaluate(() => {
+    let text = Array.from(
+      document.querySelectorAll("#video-title"),
+      (el) => el.innerText
+    );
     return text;
-  })
-console.log(arr)
-  await browser.close();
+  });
+    try {
+    const data = arr.map(i=>(i +"\n"))
+    const promise = writeFile("data.txt", data,);
+    await promise;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    console.log('All received data is written in data.txt.\nClosing the browser... ')
+    await browser.close();
+  }
+  console.log('Browser is closed')
 })();
